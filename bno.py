36bb521a061e9c1b92f67f6149ef4d0b04fae906
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-26 10:51:38 -0700

    ac

--- bno.py
+++ bno.py
@@ -8,6 +8,13 @@ from adafruit_bno08x import BNO_REPORT_ROTATION_VECTOR
 
 import json
 import websockets
+import asyncio
+
+## gloabl variables
+quat_i = 0
+quat_j = 0
+quat_k = 0
+quat_real = 1
 
 # get args
 import argparse
@@ -18,8 +25,6 @@ args = parser.parse_args()
 hand = args.hand or "RightArm"
 
 
-websockets.connect("ws://mmdongle.local:80/hub")
-
 
 i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
@@ -28,9 +33,18 @@ bno.enable_feature(BNO_REPORT_GYROSCOPE)
 bno.enable_feature(BNO_REPORT_MAGNETOMETER)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
+async def send_data():
+    uri = "ws://ws://mmdongle.local/hub"  
+    async with websockets.connect(uri) as websocket:
+        message = json.dumps({"bone": hand, "w": quat_i, "x": quat_j, "y": quat_k, "z": quat_real})
+        await websocket.send(message) 
+
+
+
+
 while True:
     quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
     print("%0.2f  %0.2f %0.2f %0.2f" % (quat_i, quat_j, quat_k, quat_real))
 
-    # send over websocket
-    websockets.send(json.dumps({"bone": hand, "w": quat_i, "x": quat_j, "y": quat_k, "z": quat_real}))
+
+asyncio.run(send_data())
