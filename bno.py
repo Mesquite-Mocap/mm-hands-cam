90c6131fc06894419a3ae94add1cab5b4b0c2b0f
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-26 12:40:46 -0700

    ac

--- bno.py
+++ bno.py
@@ -5,26 +5,16 @@ from adafruit_bno08x import BNO_REPORT_ACCELEROMETER
 from adafruit_bno08x import BNO_REPORT_GYROSCOPE
 from adafruit_bno08x import BNO_REPORT_MAGNETOMETER
 from adafruit_bno08x import BNO_REPORT_ROTATION_VECTOR
-
 import json
-import websockets
+import adafruit_websockets
 import asyncio
-
-## gloabl variables
-quat_i = 0
-quat_j = 0
-quat_k = 0
-quat_real = 1
-
-# get args
+import time
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--hand", help="host to connect to")
 args = parser.parse_args()
-
 hand = args.hand or "RightArm"
-
-
 
 i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
@@ -34,26 +24,10 @@ bno.enable_feature(BNO_REPORT_MAGNETOMETER)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 # Connect to the ws server
-
-websockets_server = "ws://192.168.1.50:80/hub"
-async def send_data():
-    async with websockets.connect(websockets_server) as websocket:
-        # send data
-        data = {
-        "bone": hand,
-        "w": quat_i,
-        "x": quat_j,
-        "y": quat_k,
-        "z": quat_real
-        }
-        await websocket.send(json.dumps(data))
-    
-        # receive response
-        response = await websocket.recv()
-        print(response)
+ws = adafruit_websockets.client.WebsocketClient("ws://mmdongle.local:80/hub")
+ws.connect()
 
 while True:
     quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
     print("%0.2f  %0.2f %0.2f %0.2f" % (quat_i, quat_j, quat_k, quat_real))
-    send_data()
-
+    ws.send_json({"bone": hand, "w": quat_i, "x": quat_j, "y": quat_k, "z": quat_real})
