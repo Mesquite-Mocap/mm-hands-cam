ac09bcdcf1e5e6b2139f4513ce4b018e380c9ac1
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-26 15:23:40 -0700

    ac

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
+const ws = require('ws');
+// client
+const client = new ws('ws://mmdongle.local:80/hub');
+client.on('open', () => {
+  console.log('connected');
+  client.send('Hello!');
+});
+client.on('message', data => {
+    console.log(data);
+    }
+);
+client.on('close', () => {
+    console.log('disconnected');
+    process.exit(0);
+    }
+);
+
+
 const Tail = require('tail-file');
 const mytail = new Tail("log", line => {
   console.log( line );
+  ws.send( line );
 });
