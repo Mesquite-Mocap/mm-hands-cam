e04b1a634204a1e68e636c365ce1a9f1ccd32f3d
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-27 20:06:29 -0700

    ac

--- bno.py
+++ bno.py
@@ -7,6 +7,15 @@ import json
 import time
 import argparse
 
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+sched = BackgroundScheduler()
+
+def sendMessage():
+    ws.send(json.dumps({"bone": hand, "x": quat_i, "y": quat_j, "z": quat_k, "w": quat_real}))
+
+
 import websocket
 
 ws = websocket.WebSocket()
@@ -23,11 +32,19 @@ hand = args.hand or "RightForeArm"
 i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+sched.add_job(sendMessage, 'interval', seconds=1000/30)
+sched.start()
 
-
+quat_i = 0
+quat_j = 0
+quat_k = 0
+quat_real = 0
 
 while True:
     quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-    ws.send(json.dumps({"bone": hand, "w": quat_i, "x": quat_j, "y": quat_k, "z": quat_real})) 
+    # ws.send(json.dumps({"bone": hand, "x": quat_i, "y": quat_j, "z": quat_k, "w": quat_real})) 
     # sleep for a 10 ms
     # time.sleep(0.03)
+
+
+
